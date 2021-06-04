1b2e8a1fce407654cb3933c82fa9173b15f35fd2
dolevsaadia <57878915+dolevsaadia@users.noreply.github.com>
2021-06-04 19:40:02 +0300

    Add files via upload

---
+++ HareAndTortoise/Node.java
@@ -0,0 +1,18 @@
+package HareAndTortoise;
+
+public class Node {
+
+    Node next;
+    int id;
+    static int unique_id = 0;
+
+    public Node() {
+        this.next = null;
+        this.id = unique_id++;
+    }
+
+    @Override
+    public String toString() {
+        return "("+this.id+")->";
+    }
+}
